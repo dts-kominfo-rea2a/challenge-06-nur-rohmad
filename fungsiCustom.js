4c69073b78f44e6eb8ca1974b17fc2809342cdfe
nur-rohmad <nurrohmad776@gmail.com>
2022-06-07 22:38:11 +0700

    mengumpulkan tugas 6

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -18,7 +18,37 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fncalback) => {
+  // require untuk funsi read data
+  const fs = require("fs");
+  // variabel untuk menampung data hasil bacaan
+  let dataKeseluruhan = [];
+  // baca data pertama
+  fs.readFile(file1, "utf8", (err, data) => {
+    // handler ketika ada error pada pembacaan data pertama
+    if (err) return fncalback(err, null);
+    // jika tidak ada error maka data akan ditampung pada variabel data1
+    const data1 = JSON.parse(data).message.slice(5);
+    // pembacaan data file ke dua
+    fs.readFile(file2, "utf8", (err, data) => {
+      //  handler ketika ada error pada pembacaan data kedua
+      if (err) return fncalback(err, null);
+      // jika tidak ada error maka data akan ditampung pada variabel data2
+      let data2 = JSON.parse(data)[0].message.slice(5);
+      // pembacaan data file ke tiga
+      fs.readFile(file3, "utf8", (err, data) => {
+        // handler ketika ada error pada pembacaan data ketiga
+        if (err) return fncalback(err, null);
+        // jika tidak ada error maka data akan ditampung pada variabel data3
+        let data3 = JSON.parse(data)[0].data.message.slice(5);
+        // push data ke dalam variabel data keseluruhan
+        dataKeseluruhan.push(data1, data2, data3);
+        // memanggil fungsi callback dengan  null untuk parameter 1 dan datakeseluruhan untuk parameter kedua
+        fncalback(null, dataKeseluruhan);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
